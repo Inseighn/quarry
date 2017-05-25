d9e6e9b7183522a642ac7822d24d7139bb0a8c11
Aaron <aaronrubesh6@gmail.com>
2017-05-25 11:32:28 -0500

    Added tests for http gets, inc. headers/params

--- spec/quarry_spec.rb
+++ spec/quarry_spec.rb
@@ -1,21 +1,35 @@
 require "spec_helper"
 require "quarry"
+require "webmock/rspec"
 
 RSpec.describe Quarry do
   it "has a version number" do
     expect(Quarry::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
 
 RSpec.describe Quarry::HTTP do 
 	it "gets a page" do
-		body = Quarry::HTTP.get("http://google.com")
-		puts body
-		expect(body).to include("<html>")
+		WebMock.reset!
+		stub =stub_request(:get, "www.example.com")
+		Quarry::HTTP.get("http://www.example.com")
+		expect(stub).to have_been_requested
 	end
+
+	it "gets a page with parameters" do
+		WebMock.reset!
+		stub = stub_request(:get, "www.example.com").with(query: {:test_param => "julius"})
+		Quarry::HTTP.get("http://www.example.com", {:test_param => "julius"})
+		expect(stub).to have_been_requested 
+	end
+
+	it "gets a page with headers" do
+		WebMock.reset!
+		stub = stub_request(:get, "www.example.com").with(headers: {:test_param => "julius"})
+		Quarry::HTTP.get("http://www.example.com", {}, {:test_param => "julius"})
+		expect(stub).to have_been_requested 
+	end
+
 end
 
